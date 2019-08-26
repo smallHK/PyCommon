1cdc54e732e9459fca85da58ff8c19814719b970
zhangyy <zhangyy@yisa.com>
2019-08-26 10:01:37 +0800

    代码重构

--- excel.py
+++ excel.py
@@ -1,36 +1,22 @@
-import xlrd
-import numpy as np
-import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+import xlrd
 
-data = None
 
-
+# 将数据封装为对象列表
 class DataSet:
 
-    def __init__(self, filename):
+    def load_data(self, file_name):
         try:
-            self.origin = xlrd.open_workbook(filename)
+            self.origin = xlrd.open_workbook(file_name)
             print("data load success!")
         except Exception as e:
             print(str(e))
 
-        self.obj_list = None
-
-        # 攻击
-        self.weapon_labels = ['Biological', 'Chemical', 'Radiological', 'Nuclear',
-                         'Firearms', 'Explosives', 'Fake Weapons', 'Incendiary',
-                         'Melee', 'Vehicle', 'Sabotage Equipment', 'Other', 'Unknown']
-
-    def get_obj_list(self):
-        if self.obj_list is not None:
-            return self.obj_list
-
+    def transfor_origin(self):
         table = self.origin.sheets()[0]
         nrows = table.nrows
         ncols = table.ncols
         colnames = table.row_values(0)
-        self.obj_list = []
         for ri in range(1, nrows):
             row = table.row_values(ri)
             if row:
@@ -38,119 +24,24 @@ class DataSet:
                 for i in range(len(colnames)):
                     app[colnames[i]] = row[i]
                 self.obj_list.append(app)
+
+    def __init__(self, file_name=None, obj_list=None):
+        self.obj_list = []
+
+        if file_name is not None:
+            self.load_data(file_name)
+            self.transfor_origin()
+
+        if obj_list is not None:
+            self.obj_list = obj_list
+
+        # 攻击
+        self.weapon_labels = ['Biological', 'Chemical', 'Radiological', 'Nuclear',
+                              'Firearms', 'Explosives', 'Fake Weapons', 'Incendiary',
+                              'Melee', 'Vehicle', 'Sabotage Equipment', 'Other', 'Unknown']
+
+    def get_obj_list(self):
         return self.obj_list
-
-
-# 打开EXCEL文件
-def open_excel(file):
-    global data
-    if data is not None:
-        return data
-    try:
-        data = xlrd.open_workbook(file)
-        print("data load success!")
-        return data
-    except Exception as e:
-        print(str(e))
-
-
-obj_list = None
-
-
-# 数据预处理
-# 将excel转化为对象list
-def preprocessing(file):
-    global obj_list
-    if obj_list is not None:
-        return obj_list
-
-    data = open_excel(file)
-    table = data.sheets()[0]
-    nrows = table.nrows
-    ncols = table.ncols
-    colnames = table.row_values(0)
-    obj_list = []
-    for rownum in range(1, nrows):
-        row = table.row_values(rownum)
-        if row:
-            app = {}
-            for i in range(len(colnames)):
-                app[colnames[i]] = row[i]
-            obj_list.append(app)
-    return obj_list
-
-
-def test(file):
-    obj_list = preprocessing(file)
-    i = 1
-    for row in obj_list:
-        if i == 100:
-            break
-        print(row)
-        i += 1
-
-
-# 分析中国十年间发生的受害目标为运输时间的数量，死亡人数，受伤人数
-# 中国，country 44
-# 十年间，iyear 2007-2017
-# 受害目标 运输，targtype1 19
-# 事件数量
-# 死亡人数 nkill
-# 受伤人数 nwound
-def china_ten_tran(file):
-    obj_list = preprocessing(file)
-    event_list = []
-    wound_count = 0
-    kill_count = 0
-    for row in obj_list:
-        if row["country"] == 44 and 2007 <= row["iyear"] < 2017 and row['targtype1'] == 19:
-            event_list.append(row)
-            if row["nwound"] is not None and row["nwound"] != 0:
-                wound_count += row["nwound"]
-
-            if row["nkill"] is not None and row["nkill"] != 0:
-                kill_count += row["nkill"]
-
-    print("受伤人数： " + str(wound_count))
-    print("直接死亡人数： " + str(kill_count))
-    event_count = len(event_list)
-    print("事件总数： " + str(event_count))
-    return event_count, wound_count, kill_count
-
-
-# 攻击方式
-# 饼状图
-def china_ten_tran_weapon_pie(file):
-    obj_list = preprocessing(file)
-    table = data.sheets()[0]
-
-    # 攻击
-    weapon_labels = ['Biological', 'Chemical', 'Radiological', 'Nuclear',
-                     'Firearms', 'Explosives', 'Fake Weapons', 'Incendiary',
-                     'Melee', 'Vehicle', 'Sabotage Equipment', 'Other', 'Unknown']
-    weapon_counts = [0 for i in range(0, len(weapon_labels))]
-
-    # 攻击方式
-    event_list = []
-    for row in obj_list:
-        if row["country"] == 44 and 2007 <= row["iyear"] < 2017 and row['targtype1'] == 19:
-            event_list.append(row)
-            if row['weaptype1'] is not None and row['weaptype1'] > 0:
-                weapon_counts[int(row['weaptype1']) - 1] += 1
-
-    labels = []
-    counts = []
-    for i in range(len(weapon_labels)):
-        if weapon_counts[i] != 0:
-            labels.append(weapon_labels[i])
-            counts.append(weapon_counts[i])
-
-    plt.figure()
-    plt.pie(counts, labels=labels, autopct='%1.2f%%')
-    plt.title("Pie chart")
-
-    plt.show()
-    plt.savefig("PieChart.jpg")
 
 
 # 条件过滤器
@@ -162,7 +53,7 @@ class DataFilter:
         for f in self._obj_filter_list:
             if f(row) is not True:
                 return False
-            return True
+        return True
 
     def add_filter(self, f):
         self._obj_filter_list.append(f)
@@ -171,61 +62,249 @@ class DataFilter:
         self._obj_filter_list = []
 
 
-# 中国
-# 受害目标：巴士 targsubtype1 99
-def china_bus(ds):
-    obj_list = ds.get_obj_list()
+class ResultSet:
+
+    def __init__(self, ds, df):
+        self.event_list = []
+        self._ds = ds
+        self._df = df
+
+    def filter(self):
+        for row in self._ds.get_obj_list():
+            if self._df.filter(row):
+                self.event_list.append(row)
+
+    def count_kill_and_wound(self):
+        wound_count = 0
+        kill_count = 0
+        for row in self.event_list:
+            if row["nwound"] is not None and row["nwound"] != 0 and row["nwound"] != '':
+                wound_count += int(row["nwound"])
+            if row["nkill"] is not None and row["nkill"] != 0 and row["nkill"] != '':
+                kill_count += int(row["nkill"])
+        print("受伤人数： " + str(wound_count))
+        print("直接死亡人数： " + str(kill_count))
+        return wound_count, kill_count
+
+    def count_event(self):
+        print("事件总数： " + str(len(self.event_list)))
+        return len(self.event_list)
+
+    def count_weapen_info(self):
+        weapon_labels = ds.weapon_labels
+        weapon_counts = [0 for i in range(len(weapon_labels))]
+        for obj in self.event_list:
+            if obj['weaptype1'] is not None and obj['weaptype1'] > 0:
+                weapon_counts[int(obj['weaptype1']) - 1] += 1
+        return weapon_labels, weapon_counts
+
+    def weapon_bar(self, topic):
+        """柱状图"""
+        weapon_labels, weapon_counts = self.count_weapen_info()
+        labels = []
+        counts = []
+        for i in range(len(weapon_labels)):
+            if weapon_counts[i] != 0:
+                labels.append(weapon_labels[i])
+                counts.append(weapon_counts[i])
+
+        plt.bar(range(len(counts)), counts, tick_label=labels)
+        plt.title(topic)
+        #plt.show()
+        plt.savefig(topic + "_bar_" +  ".png")
+        plt.show()
+
+    def weapon_pie(self, topic):
+        """扇形图"""
+        weapon_labels, weapon_counts = self.count_weapen_info()
+        labels = []
+        counts = []
+        for i in range(len(weapon_labels)):
+            if weapon_counts[i] != 0:
+                labels.append(weapon_labels[i])
+                counts.append(weapon_counts[i])
+
+        plt.figure()
+        plt.pie(counts, labels=labels, autopct='%1.2f%%')
+        plt.title(topic)
+        plt.savefig(topic + "_pie_" + ".png")
+        plt.show()
+
+    def further_filter(self, df):
+        ds = DataSet(obj_list=self.event_list)
+        return ResultSet(ds, df)
+
+
+# 受害目标 运输，targtype1 19
+# 事件数量
+# 死亡人数 nkill
+# 受伤人数 nwound
+def count_event_wound_kill(ds, country=None, targtype1=None, targsubtype1=None):
     df = DataFilter()
-    df.add_filter(lambda x: x["country"] == 44)
+    if country is not None:
+        df.add_filter(lambda x: x["country"] == country)
+    if targtype1 is not None:
+        df.add_filter(lambda x: x['targtype1'] == targtype1)
+    if targsubtype1 is not None:
+        df.add_filter(lambda x: x["targsubtype1"] == targsubtype1)
+    rs = ResultSet(ds, df)
+    rs.filter()
+    event_count = rs.count_event()
+    wound_count, kill_count = rs.count_kill_and_wound()
+    return event_count, wound_count, kill_count
+
+
+# 袭击方式分析
+# 扇形图、直方图
+def draft_pie_bar(ds, country=None, targtype1=None, targsubtype1=None, topic=None):
+    df = DataFilter()
+    if country is not None:
+        df.add_filter(lambda x: x["country"] == country)
+    if targtype1 is not None:
+        df.add_filter(lambda x: x['targtype1'] == targtype1)
+    if targsubtype1 is not None:
+        df.add_filter(lambda x: x["targsubtype1"] == targsubtype1)
+    rs = ResultSet(ds, df)
+    rs.filter()
+    rs.weapon_bar(topic)
+    rs.weapon_pie(topic)
+
+
+#
+#
+# 运输
+# 攻击方式
+def chain_tran(ds, country):
+    df = DataFilter()
+    df.add_filter(lambda x: x["country"] == country)
     df.add_filter(lambda x: x['targtype1'] == 19)
-    df.add_filter(lambda x: x["targsubtype1"] == 99)
-    bus_event_list = []
-    for row in obj_list:
-        if df.filter(row):
-            bus_event_list.append(row)
-    print("中国巴士事件数量: " + str(len(bus_event_list)))
-
-    df.empty_filter()
-    df.add_filter(lambda x: x["country"] == 44)
-    df.add_filter(lambda x: x['targtype1'] == 19)
-    tran_event_list = []
-    for row in obj_list:
-        if df.filter(row):
-            tran_event_list.append(row)
-    print("中国运输事件数量: " + str(len(tran_event_list)))
-
-    # 柱状图
-    weapon_labels = ds.weapon_labels
-    weapon_counts = [0 for i in range(len(weapon_labels))]
-    for obj in tran_event_list:
-        if obj['weaptype1'] is not None and obj['weaptype1'] > 0:
-            weapon_counts[int(obj['weaptype1']) - 1] += 1
-    labels = []
-    counts = []
-    for i in range(len(weapon_labels)):
-        if weapon_counts[i] != 0:
-            labels.append(weapon_labels[i])
-            counts.append(weapon_counts[i])
-
-    plt.bar(range(len(counts)), counts, tick_label=labels)
-    plt.title("Bar chart")
-
-    plt.show()
-    plt.savefig("BarChart.jpg")
-
-
-
-# 中国
-# 受害目标：巴士站
+    rs = ResultSet(ds, df)
+    rs.filter()
+    rs.count_event()
 
 
 # 受害目标：中国巴士、中国巴士站
+def chain_bus(ds, country):
+    df = DataFilter()
+    df.add_filter(lambda x: x["country"] == country)
+    df.add_filter(lambda x: x["targsubtype1"] == 99)
+    rs = ResultSet(ds, df)
+    rs.filter()
+    event_count = rs.count_event()
+    wound_count, kill_count = rs.count_kill_and_wound()
+    return event_count, wound_count, kill_count
+
+
+# 受害目标：中国巴士、中国巴士站
+def chain_bus_station(ds, country):
+    df = DataFilter()
+    df.add_filter(lambda x: x["country"] == country)
+    df.add_filter(lambda x: x["targsubtype1"] == 101)
+    rs = ResultSet(ds, df)
+    rs.filter()
+    event_count = rs.count_event()
+    wound_count, kill_count = rs.count_kill_and_wound()
+    return event_count, wound_count, kill_count
+
+
+# 巴士站
+# 受伤人数
+# 死亡人数
+# 攻击手段
+# 中国
+def compute_china(ds):
+    tran_event_count, tran_wound_count, tran_kill_count = count_event_wound_kill(ds, country=44, targtype1=19)
+    draft_pie_bar(ds, country=44, targtype1=19, topic="chinaTran")
+    print("")
+
+    bus_event_count, bus_wound_count, bus_kill_count = count_event_wound_kill(ds, country=44, targsubtype1=99)
+    draft_pie_bar(ds, country=44, targsubtype1=99, topic="chinaBus")
+
+
+    print("")
+
+    bus_station_event_count, bus_station_wound_count, bus_station_kill_count \
+        = count_event_wound_kill(ds, country=44, targsubtype1=101)
+    draft_pie_bar(ds, country=44, targsubtype1=101, topic="chinaBusStation")
+
+    print("")
+
+
+# 美国
+def compute_america(ds):
+    tran_event_count, tran_wound_count, tran_kill_count = count_event_wound_kill(ds, country=217, targtype1=19)
+    draft_pie_bar(ds, country=217, targtype1=19, topic="AmericaTran")
+
+
+    print("")
+
+    bus_event_count, bus_wound_count, bus_kill_count = count_event_wound_kill(ds, country=217, targsubtype1=99)
+    draft_pie_bar(ds, country=217, targsubtype1=99, topic="AmericaBus")
+
+    print("")
+
+    bus_station_event_count, bus_station_wound_count, bus_station_kill_count \
+        = count_event_wound_kill(ds, country=217, targsubtype1=101)
+    draft_pie_bar(ds, country=217, targsubtype1=101, topic="AmericaBusStation")
+    print("")
+
+
+# 法国
+def compute_france(ds):
+    tran_event_count, tran_wound_count, tran_kill_count = count_event_wound_kill(ds, country=69, targtype1=19)
+    draft_pie_bar(ds, country=69, targtype1=19, topic="FranceTran")
+    print("")
+
+    bus_event_count, bus_wound_count, bus_kill_count = count_event_wound_kill(ds, country=69, targsubtype1=99)
+    draft_pie_bar(ds, country=69, targsubtype1=99, topic="FranceBus")
+    print("")
+
+    bus_station_event_count, bus_station_wound_count, bus_station_kill_count \
+        = count_event_wound_kill(ds, country=69, targsubtype1=101)
+    draft_pie_bar(ds, country=69, targsubtype1=101, topic="FranceBusStation")
+    print("")
+
+
+# 西班牙
+def compute_spain(ds):
+    tran_event_count, tran_wound_count, tran_kill_count = count_event_wound_kill(ds, country=185, targtype1=19)
+    draft_pie_bar(ds, country=185, targtype1=19, topic="SpainTran")
+    print("")
+
+    bus_event_count, bus_wound_count, bus_kill_count = count_event_wound_kill(ds, country=185, targsubtype1=99)
+    draft_pie_bar(ds, country=185, targsubtype1=99, topic="SpainBus")
+    print("")
+
+    bus_station_event_count, bus_station_wound_count, bus_station_kill_count \
+        = count_event_wound_kill(ds, country=185, targsubtype1=101)
+    draft_pie_bar(ds, country=185, targsubtype1=101, topic="SpainBusStation")
+    print("")
+
+
+def compute_world(ds):
+    tran_event_count, tran_wound_count, tran_kill_count = count_event_wound_kill(ds, targtype1=19)
+    draft_pie_bar(ds, targtype1=19, topic="worldTran")
+    print("")
+
+    bus_event_count, bus_wound_count, bus_kill_count = count_event_wound_kill(ds, targsubtype1=99)
+    draft_pie_bar(ds, targsubtype1=99, topic="worldBus")
+    print("")
+
+    bus_station_event_count, bus_station_wound_count, bus_station_kill_count \
+        = count_event_wound_kill(ds, targsubtype1=101)
+    draft_pie_bar(ds, targsubtype1=101, topic="worldBusStation")
+    print("")
 
 
 if __name__ == "__main__":
     # test('excelData.xlsx')
     file_name = 'excelData.xlsx'
-    #china_ten_tran('excelData.xlsx')
-    #china_ten_tran_weapon_pie('excelData.xlsx')
+    # china_ten_tran('excelData.xlsx')
+    # china_ten_tran_weapon_pie('excelData.xlsx')
     ds = DataSet(file_name)
-    china_bus(ds)
+    compute_china(ds)
+    compute_america(ds)
+    compute_france(ds)
+    compute_spain(ds)
+    compute_world(ds)
+
